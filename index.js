1d3babd8f6d0696a58b664a2ff2e7ef2681cfa21
Nausic4 <addolorata.castriota@dci-student.org>
2024-12-19 10:14:46 +0100

    Creating Library

---
+++ index.js
@@ -0,0 +1,27 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+console.log("Welcome to the Shrugman! :-)");
+
+//Library with Categories:
+const movies = [
+  "The Shawshank Redemption",
+  "The Godfather",
+  "The Dark Knight",
+  "The Godfather Part II",
+  "12 Angry Men",
+];
+const books = [
+  "Anna Karenina",
+  "Madame Bovary",
+  "War and Peace",
+  "The Great Gatsby",
+  "In Search of Lost Time",
+];
+const anime = [
+  "Hunter x Hunter",
+  "Code Geass",
+  "Cowboy Bebop",
+  "Fighting Spirit",
+  "Death Note",
+];
+//-------------------------
